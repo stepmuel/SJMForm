8b95334f3c4c3ccc3005f6e6f9da3cdb2055c30e
stepmuel <stephan@heap.ch>
2016-10-18 23:30:09 +0200

    change dictionaries data structures in AST from array to object

--- SJMForm.php
+++ SJMForm.php
@@ -21,11 +21,11 @@ class SJMForm {
 		$offset = 0;
 		$tree = array();
 		while (preg_match($pattern, $fml, $m, PREG_OFFSET_CAPTURE, $offset)===1) {
-			$node = array();
-			$node['type'] = $m[1][0];
-			$node['name'] = $m[2][0];
-			$node['args'] = $this->arglist($m[3][0]);
-			if (isset($m[5])) $node['childNodes'] = $this->parse($m[5][0]);
+			$node = new stdClass();
+			$node->type = $m[1][0];
+			$node->name = $m[2][0];
+			$node->args = $this->arglist($m[3][0]);
+			if (isset($m[5])) $node->childNodes = $this->parse($m[5][0]);
 			$offset = $m[0][1] + strlen($m[0][0]);
 			$tree []= $node;
 		}
@@ -67,7 +67,7 @@ class SJMForm {
 	public function __toString() {
 		$out = '';
 		foreach ($this->tree as $node) {
-			$method = 'produce_'.$node['type'];
+			$method = 'produce_'.$node->type;
 			if(!method_exists($this, $method)) continue;
 			$out .= $this->$method($node);
 		}
@@ -78,11 +78,11 @@ class SJMForm {
 	public function preprocess($data) {
 		$out = array();
 		foreach ($this->tree as $node) {
-			$name = $node['name'];
-			if ($node['type']=='checkbox') {
+			$name = $node->name;
+			if ($node->type=='checkbox') {
 				// checkboxes don't send anything for unchecked boxes
-				foreach ($node['childNodes'] as $child) {
-					$key = "{$name}_$child[name]";
+				foreach ($node->childNodes as $child) {
+					$key = "{$name}_{$child->name}";
 					$out[$key] = isset($data[$key]);
 				}
 			} else {
@@ -96,14 +96,14 @@ class SJMForm {
 	// set form values
 	public function set($data) {
 		foreach ($this->tree as &$node) {
-			$type = $node['type'];
-			$name = $node['name'];
+			$type = $node->type;
+			$name = $node->name;
 			// special handling for checkboxes
 			if ($type=='checkbox') {
-				foreach ($node['childNodes'] as &$child) {
-					$key = "{$name}_$child[name]";
+				foreach ($node->childNodes as &$child) {
+					$key = $name.'_'.$child->name;
 					if (!isset($data[$key])) continue;
-					$child['args'][1] = $data[$key];
+					$child->args[1] = $data[$key];
 				}
 				continue;
 			}
@@ -112,76 +112,78 @@ class SJMForm {
 			$value = $data[$name];
 			// handle radio buttons and select
 			if ($type=='radio' || $type=='select') {
-				foreach ($node['childNodes'] as &$child) {
-					$child['args'][1] = $value == $child['name'];
+				foreach ($node->childNodes as $child) {
+					$child->args[1] = $value == $child->name;
 				}
 				continue;
 			}
 			// hidden fields have no display name argument
 			if ($type=='hidden') {
-				$node['args'][0] = $value;
+				$node->args[0] = $value;
 				continue;
 			}
 			// all other types have their value as second argument
-			$node['args'][1] = $value;
+			$node->args[1] = $value;
 		}
 	}
 	
 	// html generators
 	private function produce_hidden($e) {
-		if (!isset($e['args'][0])) $e['args'][0] = '';
-		return "<input type=\"hidden\" name=\"$e[name]\" value=\"".htmlentities($e['args'][0])."\" id=\"$e[name]\">\n";
+		if (!isset($e->args[0])) $e->args[0] = '';
+		return "<input type=\"hidden\" name=\"$e->name\" value=\"".htmlentities($e->args[0])."\" id=\"$e->name\">\n";
 	}
 	private function produce_text($e) {
-		if (!isset($e['args'][1])) $e['args'][1] = '';
-		$out = "<dt><label for=\"$e[name]\">".htmlentities($e['args'][0])."</label></dt>\n";
-		$out .= "<dd><input type=\"text\" name=\"$e[name]\" value=\"".htmlentities($e['args'][1])."\"  size=\"30\" id=\"$e[name]\"></dd>\n";
+		if (!isset($e->args[1])) $e->args[1] = '';
+		$out = "<dt><label for=\"$e->name\">".htmlentities($e->args[0])."</label></dt>\n";
+		$out .= "<dd><input type=\"text\" name=\"$e->name\" value=\"".htmlentities($e->args[1])."\"  size=\"30\" id=\"$e->name\"></dd>\n";
 		return $out;
 	}
 	private function produce_password($e) {
-		if (!isset($e['args'][1])) $e['args'][1] = '';
-		$out = "<dt><label for=\"$e[name]\">".htmlentities($e['args'][0])."</label></dt>\n";
-		$out .= "<dd><input type=\"password\" name=\"$e[name]\" value=\"".htmlentities($e['args'][1])."\"  size=\"30\" id=\"$e[name]\"></dd>\n";
+		if (!isset($e->args[1])) $e->args[1] = '';
+		$out = "<dt><label for=\"$e->name\">".htmlentities($e->args[0])."</label></dt>\n";
+		$out .= "<dd><input type=\"password\" name=\"$e->name\" value=\"".htmlentities($e->args[1])."\"  size=\"30\" id=\"$e->name\"></dd>\n";
 		return $out;
 	}
 	private function produce_textarea($e) {
-		if (!isset($e['args'][1])) $e['args'][1] = '';
-		$out = "<dt><label for=\"$e[name]\">".htmlentities($e['args'][0])."</label></dt>\n";
-		$out .= "<dd><textarea name=\"$e[name]\" rows=\"6\" cols=\"40\" id=\"$e[name]\">".htmlentities($e['args'][1])."</textarea></dd>\n";
+		if (!isset($e->args[1])) $e->args[1] = '';
+		$out = "<dt><label for=\"$e->name\">".htmlentities($e->args[0])."</label></dt>\n";
+		$out .= "<dd><textarea name=\"$e->name\" rows=\"6\" cols=\"40\" id=\"$e->name\">".htmlentities($e->args[1])."</textarea></dd>\n";
 		return $out;
 	}
 	private function produce_radio($e) {
-		$out = "<dt><label>".htmlentities($e['args'][0])."</label></dt>\n";
-		foreach ($e['childNodes'] as $c) {
-			if (!isset($c['args'][1])) $c['args'][1] = false;
-			$checked = ($c['args'][1]) ? ' checked' : '';
+		$out = "<dt><label>".htmlentities($e->args[0])."</label></dt>\n";
+		foreach ($e->childNodes as $c) {
+			if (!isset($c->args[1])) $c->args[1] = false;
+			$checked = ($c->args[1]) ? ' checked' : '';
+			$id = $e->name.'_'.$c->name;
 			$out .= "<dd>\n";
-			$out .= "\t<input type=\"radio\" name=\"$e[name]\" value=\"$c[name]\" id=\"$e[name]_$c[name]\"$checked>\n";
-			$out .= "\t<label for=\"$e[name]_$c[name]\">".htmlentities($c['args'][0])."</label>\n";
+			$out .= "\t<input type=\"radio\" name=\"$e->name\" value=\"$c->name\" id=\"$id\"$checked>\n";
+			$out .= "\t<label for=\"$id\">".htmlentities($c->args[0])."</label>\n";
 			$out .= "</dd>\n";
 		}
 		return $out;
 	}
 	private function produce_checkbox($e) {
-		$out = "<dt><label>".htmlentities($e['args'][0])."</label></dt>";
-		foreach ($e['childNodes'] as $c) {
-			if (!isset($c['args'][1])) $c['args'][1] = false;
-			$checked = ($c['args'][1]) ? ' checked' : '';
+		$out = "<dt><label>".htmlentities($e->args[0])."</label></dt>";
+		foreach ($e->childNodes as $c) {
+			if (!isset($c->args[1])) $c->args[1] = false;
+			$checked = ($c->args[1]) ? ' checked' : '';
+			$id = $e->name.'_'.$c->name;
 			$out .= "<dd>\n";
-			$out .= "\t<input type=\"checkbox\" name=\"$e[name]_$c[name]\" value=\"1\" id=\"$e[name]_$c[name]\"$checked>\n";
-			$out .= "\t<label for=\"$e[name]_$c[name]\">".htmlentities($c['args'][0])."</label>\n";
+			$out .= "\t<input type=\"checkbox\" name=\"$id\" value=\"1\" id=\"$id\"$checked>\n";
+			$out .= "\t<label for=\"$id\">".htmlentities($c->args[0])."</label>\n";
 			$out .= "</dd>\n";
 		}
 		return $out;
 	}
 	private function produce_select($e) {
-		$out = "<dt><label for=\"$e[name]\">".htmlentities($e['args'][0])."</label></dt>\n";
+		$out = "<dt><label for=\"$e->name\">".htmlentities($e->args[0])."</label></dt>\n";
 		$out .= "<dd>\n";
-		$out .= "\t<select size=\"1\" name=\"$e[name]\" id=\"$e[name]\">\n";
-		foreach ($e['childNodes'] as $c) {
-			if (!isset($c['args'][1])) $c['args'][1] = false;
-			$selected = ($c['args'][1]) ? ' selected=\"selected\"' : '';
-			$out .= "\t<option value=\"$c[name]\"$selected>".htmlentities($c['args'][0])."</option>\n";
+		$out .= "\t<select size=\"1\" name=\"$e->name\" id=\"$e->name\">\n";
+		foreach ($e->childNodes as $c) {
+			if (!isset($c->args[1])) $c->args[1] = false;
+			$selected = ($c->args[1]) ? ' selected=\"selected\"' : '';
+			$out .= "\t<option value=\"$c->name\"$selected>".htmlentities($c->args[0])."</option>\n";
 		}
 		$out .= "</select>\n";
 		$out .= "</dd>\n";
